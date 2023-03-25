09f2a0b16cd6ddfd4df0ce05a2ddb89cbe81daf4
Oliver Luo <lawliver@gmail.com>
2023-03-25 09:10:47 -0700

    started implementing setup page

---
+++ MunchMatch/src/setupMenu.tsx
@@ -0,0 +1,48 @@
+import ListGroup from "./setupMenuComponents/ListGroup";
+import Button from "./setupMenuComponents/Button";
+import "./setupMenu.css";
+
+function SetupMenu() {
+  let neighborhoods = ["Point Grey", "Kitsilano", "Main St"];
+
+  let cuisines = ["Japanese", "Indian", "Chinese"];
+
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+
+  const css = "./setupMenu.css";
+
+  //need to fix styling to space out
+  //   add more filters
+  //   implement button onCLick properly
+  return (
+    <div className="main">
+      <div className="heading">Munch Match</div>
+
+      <div className="list-group">
+        <ListGroup
+          items={neighborhoods}
+          heading="Region"
+          onSelectItem={handleSelectItem}
+        ></ListGroup>
+      </div>
+
+      <div className="list-group">
+        <ListGroup
+          items={cuisines}
+          heading="Cuisine"
+          onSelectItem={handleSelectItem}
+        ></ListGroup>
+      </div>
+
+      <div className="button">
+        <Button color="primary" onClick={() => console.log("Clicked")}>
+          Continue
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default SetupMenu;
